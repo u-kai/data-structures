ab58e80e43a4ff2bbbd767afafad96da051721ce
u-kai <76635578+u-kai@users.noreply.github.com>
2022-07-07 20:18:19 +0900

    clear array_deque remove

--- src/structs/arrays/array_stack.rs
+++ src/structs/arrays/array_stack.rs
@@ -26,6 +26,17 @@ impl<T: Debug + Clone + Default> ArrayStack<T> {
         self.array[i] = Some(x);
         self.n += 1;
     }
+    pub fn remove(&mut self, i: usize) -> Option<T> {
+        let x = self.array[i].take();
+        for j in i..(self.n - 1) {
+            self.array.swap(j, j + 1);
+        }
+        self.n -= 1;
+        if self.array.len() >= 3 * self.n {
+            self.resize()
+        }
+        x
+    }
     fn resize(&mut self) {
         let new_array: Vec<Option<T>> = vec![Default::default(); (self.n * 2).max(1)];
         let mut new_array = new_array.into_boxed_slice();
@@ -41,15 +52,31 @@ impl<T: Debug + Clone + Default> ArrayStack<T> {
 mod array_stack_test {
     use super::*;
     #[test]
+    fn remove_test() {
+        let mut array_stack = ArrayStack::new();
+        array_stack.add(0, "world");
+        array_stack.add(0, "hello");
+        array_stack.remove(0);
+        assert_eq!(
+            array_stack,
+            ArrayStack {
+                n: 1,
+                array: Box::new([Some("world"), None])
+            }
+        )
+    }
+    #[test]
     fn add_test() {
         let mut array_stack = ArrayStack::new();
         array_stack.add(0, "hello");
         array_stack.add(1, "world");
+        array_stack.add(0, "good");
+        array_stack.add(1, "bye");
         assert_eq!(
             array_stack,
             ArrayStack {
-                n: 2,
-                array: Box::new([Some("hello"), Some("world")])
+                n: 4,
+                array: Box::new([Some("good"), Some("bye"), Some("hello"), Some("world")])
             }
         )
     }
