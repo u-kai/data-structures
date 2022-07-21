4fd480b70903843e58292867359144b370bcebce
u-kai <76635578+u-kai@users.noreply.github.com>
2022-07-21 21:46:18 +0900

    clear remove perfect

--- src/structs/skip_lists/skip_list_sset.rs
+++ src/structs/skip_lists/skip_list_sset.rs
@@ -2,12 +2,17 @@ use std::{cell::RefCell, fmt::Debug, rc::Rc};
 
 use rand::{thread_rng, Rng};
 
-use crate::interfaces::{sset::SSet, uset};
+use crate::interfaces::sset::SSet;
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Node<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> {
     x: T,
     height: usize,
     nexts: Vec<Option<Rc<RefCell<Node<T>>>>>,
+}
+impl<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> Drop for Node<T> {
+    fn drop(&mut self) {
+        println!("node : {:?} is droped!!", self.x);
+    }
 }
 impl<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> Node<T> {
     fn new(x: T, height: usize) -> Self {
@@ -23,7 +28,6 @@ impl<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> Node<T> {
         } else {
             self.nexts.push(Some(next.clone()))
         }
-        //*self.nexts.get_mut(h).unwrap() = Some(next.clone())
     }
     fn set_none(&mut self, h: usize) {
         *self.nexts.get_mut(h).unwrap() = None
@@ -145,9 +149,6 @@ impl<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> SkipListSSe
     fn set_next(&mut self, h: usize, next: Rc<RefCell<Node<T>>>) {
         self.sentinel.borrow_mut().set_next(h, next)
     }
-    fn set_none(&mut self, h: usize) {
-        self.sentinel.borrow_mut().set_none(h)
-    }
 }
 
 impl<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> SSet<T> for SkipListSSet<T> {
@@ -160,60 +161,59 @@ impl<T: Clone + Debug + PartialEq + Eq + Default + PartialOrd + Ord> SSet<T> for
     }
     fn remove(&mut self, x: T) -> Option<T> {
         let mut removed = false;
-        if self.n == 0 {
-            return None;
-        }
-        let mut next = self.get_next(self.height());
-        if next.as_ref().unwrap().borrow().x == x {
-            removed = true;
-            let next_next = next.as_ref().unwrap().borrow().get_next(self.height());
-            if next_next.is_some() {
-                next.as_mut()
-                    .unwrap()
-                    .borrow_mut()
-                    .set_next(self.height(), next_next.as_ref().unwrap().clone());
-            }
-            if next_next.is_none() {
-                self.set_none(self.height());
-                self.sentinel.borrow_mut().height -= 1;
-            }
-            next = self.get_next(self.height());
-        }
+        let mut prev = self.sentinel.clone();
         let mut h = self.height() as isize;
         while h >= 0 {
-            if next.as_ref().unwrap().borrow().x == x {
-                removed = true;
-                let next_next = next.as_ref().unwrap().borrow().get_next(self.height());
-                if next_next.is_some() {
-                    next.as_mut()
-                        .unwrap()
-                        .borrow_mut()
-                        .set_next(self.height(), next_next.as_ref().unwrap().clone());
+            let next = prev.borrow().get_next(h as usize);
+            match next {
+                Some(next) => {
+                    let next_value = &next.borrow().x;
+                    if &x < next_value {
+                        h -= 1;
+                    }
+                    if &x > next_value {
+                        prev = next.clone();
+                    }
+                    if &x == next_value {
+                        removed = true;
+                        let next_next = next.borrow().get_next(h as usize);
+                        if next_next.is_some() {
+                            prev.borrow_mut()
+                                .set_next(h as usize, next_next.unwrap().clone());
+                        } else {
+                            prev.borrow_mut().set_none(h as usize);
+                        }
+                        h -= 1;
+                    }
                 }
-                if next_next.is_none() {
-                    self.set_none(self.height());
+                None => {
+                    h -= 1;
                 }
             }
-            let next_next = next.as_ref().unwrap().borrow().get_next(h as usize);
-            if next_next.is_some() {
-                if next_next.as_ref().unwrap().borrow().x == x {
-                    removed = true;
-                }
-                next = Some(next_next.as_ref().unwrap().clone());
-            }
-            if next_next.is_none() {
-                if h == 0 {
+        }
+        let mut height = self.height();
+        let new_sentinel = self
+            .sentinel
+            .borrow()
+            .nexts
+            .iter()
+            .filter_map(|node| {
+                if node.is_none() && height != 0 {
+                    height -= 1;
                     return None;
                 }
-                h -= 1;
-                next = self.get_next(h as usize);
-            }
-        }
+                node.clone()
+            })
+            .map(|node| Some(node))
+            .collect::<Vec<_>>();
+        self.change_height(height);
+        self.sentinel.borrow_mut().nexts = new_sentinel;
         if removed {
             self.n -= 1;
-            return Some(x);
+            Some(x)
+        } else {
+            None
         }
-        None
     }
     fn size(&self) -> usize {
         self.n
@@ -276,34 +276,51 @@ mod skip_list_sset_test {
         assert_eq!(list, tobe);
         assert!(!list.add_base(1, 4))
     }
-    //#[test]
-    //fn remove_book_test() {
-    //let mut list = SkipListSSet::new();
-    //list.add(0);
-    //println!("add = {:?}", list);
-    //assert_eq!(list.remove(0), Some(0));
-    //println!("remove = {:?}", list);
-    //list.add(1);
-    //list.add(2);
-    //list.add(3);
-    //list.add(10);
-    //assert_eq!(list.remove(0), None);
-    //assert_eq!(list.remove(1), Some(1));
-    //}
-    //#[test]
-    //fn remove_test() {
-    //let mut list = SkipListSSet::new();
-    //list.add(0);
-    //println!("add = {:?}", list);
-    //assert_eq!(list.remove(0), Some(0));
-    //println!("remove = {:?}", list);
-    //list.add(1);
-    //list.add(2);
-    //list.add(3);
-    //list.add(10);
-    //assert_eq!(list.remove(0), None);
-    //assert_eq!(list.remove(1), Some(2));
-    //}
+    #[test]
+    fn remove_test() {
+        println!("remove_test");
+        let mut list = SkipListSSet::new();
+        list.add_base(0, 0);
+        assert_eq!(list.remove(0), Some(0));
+        assert_eq!(list.remove(0), None);
+        list.add_base(0, 0);
+        list.add_base(1, 1);
+        list.add_base(2, 3);
+        list.add_base(3, 2);
+        list.add_base(7, 10);
+        list.add_base(9, 11);
+        list.add_base(10, 0);
+        assert_eq!(list.remove(1), Some(1));
+        assert_eq!(list.remove(8), None);
+        assert_eq!(list.remove(9), Some(9));
+        assert_eq!(list.remove(3), Some(3));
+        let mut sentinel = Node::new(0, 10);
+        let zero_node = Rc::new(RefCell::new(Node::new(0, 0)));
+        let two_node = Rc::new(RefCell::new(Node::new(2, 3)));
+        let seven_node = Rc::new(RefCell::new(Node::new(7, 10)));
+        let ten_node = Rc::new(RefCell::new(Node::new(10, 0)));
+        zero_node.borrow_mut().set_next(0, two_node.clone());
+        two_node.borrow_mut().set_next(0, seven_node.clone());
+        two_node.borrow_mut().set_next(1, seven_node.clone());
+        two_node.borrow_mut().set_next(2, seven_node.clone());
+        two_node.borrow_mut().set_next(3, seven_node.clone());
+        seven_node.borrow_mut().set_next(0, ten_node.clone());
+        sentinel.set_next(0, zero_node.clone());
+        sentinel.set_next(1, two_node.clone());
+        sentinel.set_next(2, two_node.clone());
+        sentinel.set_next(3, two_node.clone());
+        sentinel.set_next(4, seven_node.clone());
+        sentinel.set_next(5, seven_node.clone());
+        sentinel.set_next(6, seven_node.clone());
+        sentinel.set_next(7, seven_node.clone());
+        sentinel.set_next(8, seven_node.clone());
+        sentinel.set_next(9, seven_node.clone());
+        sentinel.set_next(10, seven_node.clone());
+        let sentinel = Rc::new(RefCell::new(sentinel));
+        let tobe = SkipListSSet { sentinel, n: 4 };
+
+        assert_eq!(list, tobe);
+    }
     #[test]
     fn find_test() {
         let mut list = SkipListSSet::new();
